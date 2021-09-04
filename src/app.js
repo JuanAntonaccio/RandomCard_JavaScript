d635776b0078c5ffc539dc56b24238ede6bfc8a7
JuanAntonaccio <jmantonaccio@gmail.com>
2021-09-04 11:12:01 +0000

    Arreglos codigo

--- src/app.js
+++ src/app.js
@@ -5,39 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let lista = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+let lista_palos = ["♦", "♥", "♠", "♣"];
+const traerNumero = valor => {
+  return Math.floor(Math.random() * valor);
+};
+
 function mostrar_carta() {
-  let lista = [
-    "A",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "Q",
-    "K"
-  ];
-  let lista_palos = ["♦", "♥", "♠", "♣"];
   let numero = document.querySelector(".numero");
-  let ran = Math.floor(Math.random() * 13);
-  let ran_palos = Math.floor(Math.random() * 4);
+  let ran = traerNumero(lista.length);
+  let ran_palos = traerNumero(lista_palos.length);
   let palo1 = document.querySelector(".palo1");
   let palo2 = document.querySelector(".palo2");
   numero.innerHTML = lista[ran];
   palo1.innerHTML = lista_palos[ran_palos];
   palo2.innerHTML = lista_palos[ran_palos];
+  let val_color = ran_palos < 2 ? "red" : "black";
 
-  if (ran_palos < 2) {
-    palo1.style.color = "red";
-    palo2.style.color = "red";
-  } else {
-    palo1.style.color = "black";
-    palo2.style.color = "black";
-  }
+  palo1.style.color = val_color;
+  palo2.style.color = val_color;
 }
 
 window.onload = function() {
